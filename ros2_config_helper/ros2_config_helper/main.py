a1e0c68f682edbe2b5e5e652a080e7b6ecec377c
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2022-10-04 10:28:34 -0300

    * Implement support for --recursive and --force. * Fix --debug support.
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- ros2_config_helper/ros2_config_helper/main.py
+++ ros2_config_helper/ros2_config_helper/main.py
@@ -33,7 +33,6 @@ def parse_args(argv=sys.argv[1:]):
         help='The profile file is generated for another computer. '
         'This avoids checking the validity of network interfaces and similar checks.'
     )
-    # TODO: Handle the next two options
     parser.add_argument(
     '--force', '-f', action='store_true', help='use existing folders or override existing files if needed')
     parser.add_argument(
@@ -46,7 +45,7 @@ def print_tb(debug):
         tb.print_exc(file=sys.stderr)
 
 
-def print_mkdir_error(exc, output_dir):
+def print_mkdir_error(exc, output_dir, debug):
     base_msg = f"Cannot create directory '{output_dir}': "
     if isinstance(exc, FileNotFoundError):
         end = 'parent directory does not exist'
@@ -55,15 +54,15 @@ def print_mkdir_error(exc, output_dir):
     else:
         end = str(exc)
     print(f'{base_msg}{end}', file=sys.stderr)
-    print_tb()
+    print_tb(debug)
 
 
 def main(args):
-    output_dir = args.output_dir.resolve()
+    output_dir: Path = args.output_dir.resolve()
     try:
-        output_dir.mkdir(mode=0o775)
+        output_dir.mkdir(mode=0o775, parents=args.recursive, exist_ok=args.force)
     except Exception as exc:
-        print_mkdir_error(exc, output_dir)
+        print_mkdir_error(exc, output_dir, args.debug)
         return 1
     discovery_config = create_discovery_config_from_questions(args.remote_computer)
     backends = args.backends
@@ -72,18 +71,18 @@ def main(args):
     for backend_name in backends:
         backend_output_dir = output_dir / backend_name
         try:
-            backend_output_dir.mkdir(mode=0o775)
+            backend_output_dir.mkdir(mode=0o775, exist_ok=args.force)
         except Exception as exc:
-            print_mkdir_error(exc, backend_output_dir)
+            print_mkdir_error(exc, backend_output_dir, args.debug)
             return 1
         output_file = backend_output_dir / 'profiles.xml'
         try:
-            output_file.open('x')
+            output_file.open('w' if args.force else 'x')
         except Exception as exc:
             print(
                 f"Unexpected error when opening file: {exc}",
                 file=sys.stderr)
-            print_tb()
+            print_tb(args.debug)
             return 1
         backend = BACKENDS[backend_name]
         try:
@@ -95,7 +94,7 @@ def main(args):
                 'Unexpected error when generating XML profiles for backend '
                 f"{backend_name}': {exc}",
                 file=sys.stderr)
-            print_tb()
+            print_tb(args.debug)
             return 1
     return 0
 
